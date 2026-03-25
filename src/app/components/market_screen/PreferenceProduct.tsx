b2571a70959de6272554c7830c919c4923500005
TekinDabaj <tekin_dabaj@hotmail.com>
2026-03-25 13:01:52 +0300

    office pc update

--- src/app/components/market_screen/PreferenceProduct.tsx
+++ src/app/components/market_screen/PreferenceProduct.tsx
@@ -201,7 +201,7 @@ export const PreferenceProduct = React.memo(
     }, [fetchProductDetails]);
 
     /**
-     * Force refresh
+     * Force refresh.
      */
     const refresh = useCallback(async () => {
       // ✅ Clear local cache FIRST
